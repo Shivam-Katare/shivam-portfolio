064eaaceb8e791fea8a36cdd83b8f52eb1aba936
Shivam-Katare <katare27451@gmail.com>
2025-01-03 15:39:12 +0530

    Refine metadata titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,33 +13,17 @@ import Favicon from '../public/favicon.ico'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Shivam Katare | Frontend Engineer',
-    template: '%s | Shivam Katare',
+    default: 'Shivam Katare',
+    template: 'Shivam Katare',
   },
   description: 'Frontend Engineer, and a Technical Writer.',
   openGraph: {
-    title: 'Shivam Katare | Frontend Engineer',
+    title: 'Shivam Katare',
     description: 'Frontend Engineer, and a Technical Writer.',
     url: baseUrl,
     siteName: 'Shivam Katare',
     locale: 'en_US',
     type: 'website',
-    images: [
-      {
-        url: `${baseUrl}/og-image.jpg`,
-        width: 1200,
-        height: 630,
-        alt: 'Shivam Katare Portfolio Banner',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Shivam Katare | Frontend Engineer',
-    description: 'Frontend Engineer, and a Technical Writer.',
-    site: '@Shivamkatare_27',
-    creator: '@Shivamkatare_27',
-    images: [`${baseUrl}/twitter-image.jpg`],
   },
   robots: {
     index: true,
